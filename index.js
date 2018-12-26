cdd3b184f598c6370e25ad688ab7fa4a2e32c4f9
charlie  lee <ctlee9096@gmail.com>
2018-12-26 03:16:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ this.name = name;
 this.homeState = homeState;
 this.training = training;
 this.veto = function() {
-console.log(`No, I must disagree`);
+console.log(`"No, I must disagree"`);
 }
 this.approve = function() {
-console.log(`You can do that!`);
+console.log(`"You can do that!""`);
 }
 this.doCharity = function () {
 console.log(`I like to help people.`);
